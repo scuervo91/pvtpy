e125a505cf5ec4ee62db7226941c78fc667c6837
Santiago Cuervo <scuervo91@gmail.com>
2021-06-16 08:33:20 -0500

    update

--- pvtpy/fluids/fluids.py
+++ pvtpy/fluids/fluids.py
@@ -1,6 +1,9 @@
 from pydantic import BaseModel, Field
 from typing import List, Optional
 from ..pvt import PVT, TemperatureUnits,PressureUnits
+from ..black_oil.correlations import n2_correction, co2_correction, h2s_correction, pb, rs, \
+    bo, rho_oil, co, muod, muo,rsw, bw, cw, muw, rhow, rhog, z_factor, bg, eg, critical_properties,\
+        critical_properties_correction, cg
 
 class InitialConditions(BaseModel):
     pressure: float = Field(...,gt=0)
