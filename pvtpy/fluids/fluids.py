4379c4e54ec2e9b192324edf512f4d35da00f5f9
scuervo <scuervo91@gmail.com>
2021-06-18 18:51:48 -0500

    add fluidbase

--- pvtpy/fluids/fluids.py
+++ pvtpy/fluids/fluids.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel, Field
 from typing import List, Optional
-from ..pvt import PVT, TemperatureUnits,PressureUnits
+from ..pvt import PVT, TemperatureUnits,PressureUnits, Chromatography
 from ..black_oil.correlations import n2_correction, co2_correction, h2s_correction, pb, rs, \
     bo, rho_oil, co, muod, muo,rsw, bw, cw, muw, rhow, rhog, z_factor, bg, eg, critical_properties,\
         critical_properties_correction, cg
@@ -12,11 +12,14 @@ class InitialConditions(BaseModel):
     temperature_unit: PressureUnits = TemperatureUnits.farenheit
 
 
-class Oil(BaseModel):
+class FluidBase(BaseModel):
+    initial_conditions: InitialConditions = Field(...)
+    pvt: PVT = Field(None)
+    Chromatography: Chromatography = Field(None)
+
+
+class Oil(FluidBase):
     api: float = Field(...,gt=0)
     sulphur: float = Field(None, gt=0)
     bubble_pressure: float = Field(None, gt=0)
     rsb: float = Field(None, gt=0)
-    initial_conditions: InitialConditions = Field(...)
-    pvt: PVT = Field(None)
-
