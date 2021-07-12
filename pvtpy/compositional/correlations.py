682e05cc76b4a986c1da03fb94b13bf9717aeac0
scuervo <scuervo91@gmail.com>
2021-07-12 15:13:21 -0500

    add whitson coo

--- pvtpy/compositional/correlations.py
+++ pvtpy/compositional/correlations.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+from enum import Enum
 #Accentric Factor
 
 def accentric_factor(pc,tc,tb):
@@ -19,8 +19,12 @@ def accentric_factor(pc,tc,tb):
 
 # Equilibrium rations Correlations
 
-def k_wilson(pc,tc,p,t,omega):
-    """Calculate Equilibrium rations from Wilson Correlation
+class k_correlations(str,Enum):
+    wilson = 'wilson'
+    whitson = 'whitson'
+
+def k_wilson(pc,tc,p,t,omega, pk=None, method='wilson'):
+    """Calculate Equilibrium rations Correlations
 
     Args:
         pc (float): Critical Pressure [psi]
@@ -28,10 +32,16 @@ def k_wilson(pc,tc,p,t,omega):
         p (float): System Pressure [psi]
         t (float): System Temperature [°R]
         omega (float): Accentric Factor [-]
+        pk (float): Convergence Pressure (only used on Whitson correlation) [psi]
 
     Returns:
         [float]: Equibrium rations [-]
     """
-    
-    return (pc/p)*np.exp(5.37*(1+omega)*(1-(tc/t)))
+    if method == 'wilson':
+        return (pc/p)*np.exp(5.37*(1+omega)*(1-(tc/t)))
+    if method == 'whitson':
+        if pk is None:
+            raise ValueError('pk must be specified for Whitson correlation')
+        A = 1 - np.power(p/pk,0.7)
+        return np.power(pc/pk,A-1)*(pc/p)*np.exp(5.37*A*(1+omega)*(1-(tc/t)))
 
